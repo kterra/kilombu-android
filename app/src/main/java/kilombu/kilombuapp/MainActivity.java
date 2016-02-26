2715afd2f98e44903b1d1ee91b24f481f642686a
Kizzy Fernanda Terra Ferreira dos Reis <kizzyterra@gmail.com>
2016-02-25 23:52:49 -0300

    saving the day

--- app/src/main/java/kilombu/kilombuapp/MainActivity.java
+++ app/src/main/java/kilombu/kilombuapp/MainActivity.java
@@ -21,6 +21,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
